883deffaa8af8f0302c8c441958904a7cc86a6b2
”Denis <a73914862@yandex.ru>
2020-09-16 14:55:30 +0700

    +db connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express        = require('express');
+const db             = require('./config/db');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
 const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
 
-app.listen(port, () => {
-  console.log('Server is running on port ' + port);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+                        
+    // Make sure you add the database name and not the collection name
+    const db = database.db("note-api")
+    require('./app/routes')(app, db);
+
+    app.listen(port, () => {
+        console.log('Server is running on port ' + port);
+      });
+    })
